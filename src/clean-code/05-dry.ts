d572abb3ce3a0db0c30180dc8da504533a275ab4
josejulianmtnez <josejulianmdc@gmail.com>
2026-05-08 00:10:49 -0600

    feat: noDry implementation

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -1,13 +1,25 @@
-
+type Size = '' | 'S' | 'M' | 'XL'
 
 class Product {
     constructor(
-        public name: string,
+        public name: string = '',
+        public price: number = 0,
+        public size: Size = '',
     ){}
+
+    toString() {
+        // No dry
+        if ( this.name.length <= 0) throw Error('name is empty');
+        if ( this.price <= 0) throw Error('price is empty');
+        if ( this.size.length <= 0) throw Error('size is empty');
+
+        return `${ this.name } (${ this.price }), ${ this.size }`;
+    }
 }
 
+
 (()=>{
-    const bluePants = new Product('Blue Large Pants');
-    console.log(bluePants)
+    const bluePants = new Product('Blue Large Pants', 100, 'M');
+    console.log(bluePants.toString())
 })();
 
