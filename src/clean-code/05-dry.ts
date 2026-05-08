d4af31547a1fcefe2f8fabef8e5d999db274dd2d
josejulianmtnez <josejulianmdc@gmail.com>
2026-05-08 00:30:25 -0600

    feat: refactor Product class to use isProductReady for validation

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,11 +7,27 @@ class Product {
         public size: Size = '',
     ){}
 
+    isProductReady(): boolean {
+        for ( const key in this ) {
+            switch(typeof this[key]){
+                case 'string':
+                    if ( (<string><unknown>this[key]).length <= 0 ) throw Error(`${key} is empty`)
+                break;
+                case 'number':
+                    if ( (<number><unknown>this[key]) <= 0 ) throw Error(`${key} is zero`)
+                break;
+            default:
+                throw Error(`${typeof this[key]} is not valid`)
+            }
+        }
+
+        return true;
+    }
+
     toString() {
-        // No dry
-        if ( this.name.length <= 0) throw Error('name is empty');
-        if ( this.price <= 0) throw Error('price is empty');
-        if ( this.size.length <= 0) throw Error('size is empty');
+
+        if ( !this.isProductReady ) return;
+
 
         return `${ this.name } (${ this.price }), ${ this.size }`;
     }
@@ -19,7 +35,7 @@ class Product {
 
 
 (()=>{
-    const bluePants = new Product('Blue Large Pants', 100, 'M');
+    const bluePants = new Product('Blue Prints');
     console.log(bluePants.toString())
 })();
 
